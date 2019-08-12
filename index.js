31947a44abd7f9e3b35a13e695b37822ae8c2707
shashi shekhar <shashishekhar@shashis-MacBook-Air.local>
2019-08-12 15:25:35 +0530

    export modified

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TextEditor from "./src/TextEditor";
+import RichText from "./src/RichTextRenderer";
+export { TextEditor, RichText };
